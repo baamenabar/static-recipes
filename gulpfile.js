53b7a2813ce752658c81d4743f69cece45c3bdfa
Agustin Amenabar <agustin.amenabar@westwing.de>
2016-03-16 08:16:17 +0100

    adding basic image handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var argv = require('yargs').argv;
 var ssg = require('gulp-ssg');
 var rename = require('gulp-rename');
 var data = require('gulp-data');
@@ -9,7 +10,9 @@ var textile = require('gulp-textile');
 var wrap = require('gulp-wrap');
 var handlebars = require('handlebars');
 var partialLoader = require('partials-loader');
-var argv = require('yargs').argv;
+var responsive = require('gulp-responsive-images');
+var imagemin = require('gulp-imagemin');
+var imageminMozjpeg = require('imagemin-mozjpeg');
 var del = require('del');
 var fs = require('fs');
 
@@ -54,6 +57,43 @@ gulp.task('html', function(done) {
 
         // Output to build directory
         .pipe(gulp.dest('public/'));
+});
+
+gulp.task('imgs', function () {
+  gulp.src('src/imgs/**/*')
+    .pipe(responsive({
+      '*.jpg': [{
+        width: 320,
+        suffix: '-320'
+      },
+      {
+        width: 320 * 2,
+        suffix: '-320-2x'
+      },
+      {
+        width: 600,
+        suffix: '-600'
+      },
+      {
+        width: 600 * 2,
+        suffix: '-600-2x'
+      },
+      {
+        width: 900,
+        suffix: '-900'
+      },
+      {
+        width: 900 * 2,
+        suffix: '-900-2x'
+      }]
+      //,'*.png': [{width: 900,crop: true}]
+    }))
+
+    .pipe(imageminMozjpeg({quality: 85})())
+
+    //.pipe(imageminJpegoptim({progressive: true})())
+
+    .pipe(gulp.dest('public/imgs'));
 });
 
 
